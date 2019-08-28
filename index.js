91d8e446297587d41c1ca9ac8d5867bcea1e7c23
Mohammad Hadi Aliakbar <hadi.aliakbar@gmail.com>
2019-08-28 11:46:52 +0430

    with state but no filter

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import { link } from 'fs';
+
 const root = document.getElementById('root');
 
-function createForm() {
+function Form({ onsubmit }) {
   const form = document.createElement('form');
   const input = document.createElement('input');
   const submit = document.createElement('button');
@@ -10,38 +12,48 @@ function createForm() {
 
   form.onsubmit = function(event) {
     event.preventDefault();
-    addTodo(event.target[0].value);
+    onsubmit(event.target[0].value);
   };
   return form;
 }
 
-const ul = document.createElement('ul');
-
-function addTodo(text) {
-  const li = document.createElement('li');
-  li.textContent = text;
-
-  li.onclick = function() {
-    if (li.className === 'completed') {
-      li.className = '';
-    } else {
-      li.className = 'completed';
-    }
-  };
-  ul.appendChild(li);
+function List({ todos, toggleTodo }) {
+  const ul = document.createElement('ul');
+  for (const todo of todos) {
+    const li = ListItem({ todo, toggleTodo });
+    ul.appendChild(li);
+  }
+  return ul;
 }
 
-function createFooter() {
+function ListItem({ todo, toggleTodo }) {
+  const li = document.createElement('li');
+  li.textContent = todo.text;
+  li.className = todo.completed ? 'completed' : '';
+  li.onclick = () => toggleTodo(todo);
+  return li;
+}
+
+function FooterItem({ name, value, active, changeFilter }) {
+  const button = document.createElement('button');
+  button.textContent = name;
+  button.onclick = () => changeFilter(value);
+  if (value === active) {
+    button.className = 'active';
+  }
+  return button;
+}
+
+function Footer(props) {
   const div = document.createElement('div');
 
-  const all = document.createElement('button');
-  all.textContent = 'All';
-
-  const todo = document.createElement('button');
-  todo.textContent = 'Todo';
-
-  const completed = document.createElement('button');
-  completed.textContent = 'Completed';
+  const all = FooterItem({ ...props, name: 'All', value: 'all' });
+  const todo = FooterItem({ ...props, name: 'Todo', value: 'todo' });
+  const completed = FooterItem({
+    ...props,
+    name: 'Completed',
+    value: 'completed'
+  });
 
   div.appendChild(all);
   div.appendChild(todo);
@@ -51,11 +63,71 @@ function createFooter() {
 }
 
 class TodoApp {
+  constructor() {
+    this.state = {
+      todos: [],
+      visibilityFilter: 'all'
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.changeFilter = this.changeFilter.bind(this);
+    this.toggleTodo = this.toggleTodo.bind(this);
+  }
+
+  setState(partialState) {
+    this.state = { ...this.state, ...partialState };
+    render();
+  }
+
+  addTodo(text) {
+    this.setState({
+      todos: [...this.state.todos, { text, completed: false }]
+    });
+  }
+
+  toggleTodo(inputTodo) {
+    const newTodos = [];
+
+    for (const todo of this.state.todos) {
+      if (todo === inputTodo) {
+        newTodos.push({ text: todo.text, completed: !todo.completed });
+      } else {
+        newTodos.push(todo);
+      }
+    }
+    this.setState({ todos: newTodos });
+    // this.setState({
+    //   todos: this.state.todos.map(x =>
+    //     x === todo ? { ...x, completed: !x.completed } : x
+    //   )
+    // });
+  }
+
+  changeFilter(visible) {
+    this.setState({ visibilityFilter: visible });
+  }
+
+  filterList() {
+    switch (this.state.visibilityFilter) {
+      case 'all':
+        return this.state.todos;
+      case 'todo':
+        return this.state.todos.filter(x => !x.completed);
+      case 'completed':
+        return this.state.todos.filter(x => x.completed);
+    }
+  }
   render() {
     const div = document.createElement('div');
-    div.appendChild(createForm());
-    div.appendChild(ul);
-    div.appendChild(createFooter());
+    div.appendChild(Form({ onsubmit: this.addTodo }));
+    div.appendChild(
+      List({ todos: this.filterList(), toggleTodo: this.toggleTodo })
+    );
+    div.appendChild(
+      Footer({
+        active: this.state.visibilityFilter,
+        changeFilter: this.changeFilter
+      })
+    );
     return div;
   }
 }
